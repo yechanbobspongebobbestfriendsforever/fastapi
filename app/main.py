576ac1a729be50a2364bf8c6ddf15f5dec1222ef
yck <yechank06@gmail.com>
2024-03-19 16:40:19 -0700

    push to heroku

--- app/main.py
+++ app/main.py
@@ -27,5 +27,5 @@ app.include_router(vote.router)
 
 @app.get('/')
 def root():
-    return {"message":"hello world from Ubuntu!!!!!"}  
+    return {"message":"hello world successfully deployed from CI/CD!!!!!"}  
  
